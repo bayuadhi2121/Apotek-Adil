7809abe252bc4709fd9103f81ffea9f1ffa67310
sri anggraini <anggrainisri225@gmail.com>
2023-09-29 16:38:10 +0800

    View resep

--- resources/views/pages/Resep.blade.php
+++ resources/views/pages/Resep.blade.php
@@ -6,8 +6,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="text-center text-white">
-                    <span class="heading-xxs letter-spacing-xl"
-                        style="font-size: 18px; font-family: 'Poppins', sans-serif; font-weight: 600; letter-spacing: 0.5px">Enjoy
+                    <span class="heading-xxs letter-spacing-xl" style="font-size: 18px; font-family: 'Poppins', sans-serif; font-weight: 600; letter-spacing: 0.5px">Enjoy
                         Our Final Year Discount Up to 30% 👀🤑🎉</span>
                 </div>
             </div>
@@ -35,58 +34,81 @@
                         <span class="">Card title</span>
                     </div>
                     <div class="statustrx align-items-center px-lg-3 py-lg-1 px-sm-3 py-sm-1">
-                        <span class="status">pengambilan</span>
+                        <span class="status">diterima</span>
                     </div>
                 </div>
+                <div class="" style="font-size: 10px;">
+                    <span class="text-secondary">9 juli 2023, 02.50</span>
+                </div>
+                <div class="d-flex justify-content-start mt-3 ">
+                    <div class="col-lg-3" style="margin-top: 3;">
+                        <img src="{{asset('image/detail.png')}}" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" />
+                    </div>
+                    <div class="justify-content-between align-items-center px-lg-3 px-sm-1 px-md-3 mt-5">
+                        <h6 class="fw-bold">Resep Anda diTerima</h6>
+                    </div>
 
-                <div class=" trx justify-content-between align-items-center px-lg-5 px-sm-5 px-md-5">
-                    <h5 class="title">Card title</h5>
-                    <span class="tgltrx">9 juli 2023, 02.50</span>
+                </div>
+                <div class="align-items-center mt-2">
+                    <p class="catat" style="font-size: 12px;">Catatan:</p>
+                    <span class="text-secondary" style="font-size: 10px;">9 juli 2023, 02.50</span>
+                </div>
+            </div>
+            <div class="cardtrx py-3 col-md-5 col-12 ">
+                <div class="d-flex justify-content-between ">
+                    <div class="align-items-ce">
+                        <span class="">Card title</span>
+                    </div>
+                    <div class="statustrx align-items-center px-lg-3 py-lg-1 px-sm-3 py-sm-1">
+                        <span class="status">diterima</span>
+                    </div>
+                </div>
+                <div class="" style="font-size: 10px;">
+                    <span class="text-secondary">9 juli 2023, 02.50</span>
+                </div>
+                <div class="d-flex justify-content-start mt-3 ">
+                    <div class="col-lg-3" style="margin-top: 3;">
+                        <img src="{{asset('image/detail.png')}}" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" />
+                    </div>
+                    <div class="justify-content-between align-items-center px-lg-3 px-sm-1 px-md-3 mt-5">
+                        <h6 class="fw-bold">Resep Anda diTerima</h6>
+                    </div>
 
                 </div>
-            </div>
-            <div class="cardtrx py-3 col-md-5 col-12">
-                <div class="row ps-3 py-1">
-                    <div class="col-5">
-                        <h5 class="title">Card title</h5>
-                    </div>
+                <div class="align-items-center mt-2">
+                    <p class="catat" style="font-size: 12px;">Catatan:</p>
+                    <span class="text-secondary" style="font-size: 10px;">9 juli 2023, 02.50</span>
                 </div>
-                <div class=" trx justify-content-between align-items-center px-lg-5 px-sm-5 px-md-5">
-                    <h5 class="title">Card title</h5>
-                    <span class="tgltrx">9 juli 2023, 02.50</span>
+            </div>
+            <div class="cardtrx py-3 col-md-5 col-12 ">
+                <div class="d-flex justify-content-between ">
+                    <div class="align-items-ce">
+                        <span class="">Card title</span>
+                    </div>
                     <div class="statustrx align-items-center px-lg-3 py-lg-1 px-sm-3 py-sm-1">
-                        <span class="status">pengambilan</span>
+                        <span class="status">diterima</span>
                     </div>
+                </div>
+                <div class="" style="font-size: 10px;">
+                    <span class="text-secondary">9 juli 2023, 02.50</span>
+                </div>
+                <div class="d-flex justify-content-start mt-3 ">
+                    <div class="col-lg-3" style="margin-top: 3;">
+                        <img src="{{asset('image/detail.png')}}" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" />
+                    </div>
+                    <div class="justify-content-between align-items-center px-lg-3 px-sm-1 px-md-3 mt-5">
+                        <h6 class="fw-bold">Resep Anda diTerima</h6>
+                    </div>
+
+                </div>
+                <div class="align-items-center mt-2">
+                    <p class="catat" style="font-size: 12px;">Catatan:</p>
+                    <span class="text-secondary" style="font-size: 10px;">9 juli 2023, 02.50</span>
                 </div>
             </div>
-            <div class="cardtrx py-3 col-md-5 col-12">
-                <div class="row ps-3 py-1">
-                    <div class="col-5">
-                        <h5 class="title">Card title</h5>
-                    </div>
-                </div>
-                <div class=" trx justify-content-between align-items-center px-lg-5 px-sm-5 px-md-5">
-                    <h5 class="title">Card title</h5>
-                    <span class="tgltrx">9 juli 2023, 02.50</span>
-                    <div class="statustrx align-items-center px-lg-3 py-lg-1 px-sm-3 py-sm-1">
-                        <span class="status">pengambilan</span>
-                    </div>
-                </div>
-            </div>
-            <div class="cardtrx py-3 col-md-5 col-12">
-                <div class="row ps-3 py-1">
-                    <div class="col-5">
-                        <h5 class="title">Card title</h5>
-                    </div>
-                </div>
-                <div class=" trx justify-content-between align-items-center px-lg-5 px-sm-5 px-md-5">
-                    <h5 class="title">Card title</h5>
-                    <span class="tgltrx">9 juli 2023, 02.50</span>
-                    <div class="statustrx align-items-center px-lg-3 py-lg-1 px-sm-3 py-sm-1">
-                        <span class="status">pengambilan</span>
-                    </div>
-                </div>
-            </div>
+
+
+
         </div>
     </div>
 </div>
